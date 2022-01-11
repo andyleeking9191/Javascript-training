09fa1fa5bdc1c5e38c09bbc1b511562ef9882208
Andrew <andyleeking9191@yahoo.com>
2022-01-10 21:38:01 -0500

    added parsInt info

---
+++ parseInt.js
@@ -0,0 +1,11 @@
+/*
+parseInt is a function that takes in a string contianing numbers and converts it to an integer.
+parseFloat will convert to a float.
+*/
+
+const myString = '12345';
+
+const myInteger = parseInt(myString)
+
+
+console.log(typeof(myInteger))
